ea64a26991a1ba655cf5e5f44a2825090bff97d6
Hypo <joshua.klarich@gmail.com>
2015-05-12 17:57:54 -0700

    Built the Title Scene
    Created the title scene and made some really cheap interface art that
    can be quickly updated.

---
+++ CommandersCrucible_Prototype/Assets/Scripts/UIScripts/WindowLoginController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WindowLoginController : MonoBehaviour {
+
+    CanvasGroup canvas;
+
+    void Awake()
+    {
+        canvas = this.GetComponent<CanvasGroup>();
+        canvas.alpha = 0;
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+    }
+
+    public void OnClick()
+    {
+        if (canvas.alpha == 0)
+            canvas.alpha = 1;
+        else
+            canvas.alpha = 0;
+
+        if (canvas.interactable == true)
+            canvas.interactable = false;
+        else
+            canvas.interactable = true;
+
+        if (canvas.blocksRaycasts == true)
+            canvas.blocksRaycasts = false;
+        else
+            canvas.blocksRaycasts = true;
+    }
+
+}
